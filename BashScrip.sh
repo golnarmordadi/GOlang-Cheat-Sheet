055ccbeffc21c354e2da4917daf7754a8359519b
hakimemordadi <golnarmordadi@gmail.com>
2023-02-28 07:49:27 +0330

    first commit

---
+++ BashScrip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##############################################################################
+# Main Commands
+##############################################################################
+
+go env  #To see configuration like operating system and processor architecture and etc.
+Ctrl+Shift+Q  #Close Treminal
+Ctrl+Shift+T  #Add Tab
+Ctrl+Shift+W  #Delete Tab
+Ctrl ++       #Zoom In
+Ctrl --       #Zoom Out
+Ctrl H        #show hidden files on a folder
+
+##############################################################################
+# Internal Commands
+##############################################################################
+
+
+
+
+       
+
+
+
