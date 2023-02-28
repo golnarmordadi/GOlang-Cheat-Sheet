0b7c8fa779920fe4b5a5bbceb350524cac71f927
Golnar Mordadi <golnarmordadi@gmail.com>
2023-02-28 21:16:06 +0330

    ➖ Delete more lines

--- BashScrip.sh
+++ BashScrip.sh
@@ -8,7 +8,6 @@ Install Go form https://go.dev/doc/install
 Go Playground https://go.dev/play/
 Search for existing Gp packages  https://pkg.go.dev/
 ```
-
 go env  #To see configuration like operating system and processor architecture and etc
 go mod init [PACKAGE_NAME]  #Creates a go.mod file to track your code's dependencies
 go build filename #Define target by the main file
@@ -32,7 +31,6 @@ go install *.go
 go install file-1.go file-2.go ...
 ```
 
-
 ##############################################################################
 # Modules
 ##############################################################################
@@ -47,14 +45,12 @@ git push -u origin main
 ```
 
 Publish First Version
-
 ```
 git tag v1.0.0
 git push --tags
 ```
 
-Using Self Created Module
-
+Using Self Created Module into the code
 ```
 import (
 	"github.com/hakimemordadi/first"
@@ -62,25 +58,21 @@ import (
 ```
 
 Getting & Installing a Module by Path
-
 ```
 go get path/to/module  
 ```
 
 Getting the modules really "used" by the application are listed by
- 
 ```
 go list -m all
 ```
 
 Tidy makes sure go. mod matches the source code in the module. It adds any missing modules necessary to build the current module's packages and dependencies, and it removes unused modules that don't provide any relevant packages. It also adds any missing entries to go.
-
 ```
 go mod tidy
 ```
 
 Using for purposes of Unit Testing and Microbenchmark.
-
 ```
 go test
 ```
@@ -94,6 +86,7 @@ Using for code formatting
 ```
 go fmt
 ```
+
 To display documents or serve them over HTTP
 ```
 godoc
@@ -103,38 +96,47 @@ To rename variables, functions, etc
 ```
 gorename
 ```
+
 It provides the possibility of calling C language codes within Go codes
 ```
 cgo
 ```
+
 It is used to convert Go codes to assembly.
 ```
 asm
 ```
+
 Helps build, test, deploy and distribute code.
 ```
 dist
 ```
+
 It detects the use of old APIs in the application and replaces them with new APIs.
 ```
 fix
 ```
+
 Converts the output of Go tests to Jason format.
 ```
 test2json
 ```
+
 A simplified version of the ar utility on Unix.
 ```
 pack
 ```
+
 It separates executable files.
 ```
 objdump
 ```
+
 Tracks trace files.
 ```
 trace
 ```
+
 Formats your code as per the Go recommendations
 ```
 gofmt
