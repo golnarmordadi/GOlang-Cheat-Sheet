c04286edec8e846e7269bdc4dec43a954e72e46f
Golnar (H).Mordadi <golnarmordadi@gmail.com>
2023-02-28 11:11:18 +0330

    added more info

--- BashScrip.sh
+++ BashScrip.sh
@@ -9,8 +9,7 @@ go build filename #Define target by the main file
 go build . #Define target by the source folder
 go run filename #Run the code by the main file
 go run .       #Run the code by the source folder
-Ctrl --       #Zoom Out
-Ctrl H        #show hidden files on a folder
+
 
 ##############################################################################
 # Modules
@@ -18,18 +17,34 @@ Ctrl H        #show hidden files on a folder
 For making our module available to another users we need to put it on Github
 
 ```
-$ git init
-$ git add .
-$ git commit -m &quot:tada: First commit&quot
-$ git push -u origin main
+git init
+git add .
+git commit -m &quot:tada: First commit&quot
+git push -u origin main
 ```
-go get path/to/module     #Getting a module by module path
-fmt       #I/O operation
 
+Publish First Version
 
+```
+git tag v1.0.0
+git push --tags
+```
 
+Using Self Created Module
 
-       
+```
+import (
+	"github.com/hakimemordadi/first"
+)
+```
+
+Getting a Module by Path
+
+```
+go get path/to/module  
+```
+
+   
 
 
 
